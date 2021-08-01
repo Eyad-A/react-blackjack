3dd820a09dd6f0a810a1cf1715e812f7dc5b67a6
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-07-31 20:29:43 -0500

    Add navbar to blackjack page

--- src/Blackjack.js
+++ src/Blackjack.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import axios from 'axios';
 import Hand from './Hand';
 import update from 'immutability-helper';
@@ -131,6 +132,20 @@ class Blackjack extends Component {
   render() {
     return (
       <div className="blackjack-page">
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container px-5">
+            <a className="navbar-brand" href="index.html">React Blackjack</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/blackjack">Play</Link></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
         <h1 className="top-section">Blackjack</h1>
         <div className="center">
           <p className="game-status">{this.state.gameStatus}</p>
