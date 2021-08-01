6a9997c93a4672c3fecb406c909431a65a56bac3
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-08-01 00:50:25 -0500

    Use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
+        <Router basename={process.env.PUBLIC_URL}>
           <div>
             <Routes />
           </div>
-        </BrowserRouter>        
+        </Router>        
       </div>
     );
   }
