3dc7c87072bb2c8ba7ffd9fc9f62ddca124e8be4
EYAD ALSHAFI <EYAD.ALSHAFI@GMAIL.COM>
2021-07-31 20:19:01 -0500

    Add homepage bio

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { FaHandPointRight } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 import './Homepage.css';
 
 class Homepage extends Component {
@@ -13,8 +17,8 @@ class Homepage extends Component {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li className="nav-item"><a className="nav-link" href="index.html">Home</a></li>
-                <li className="nav-item"><a className="nav-link" href="about.html">Play</a></li>
+                <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/blackjack">Play</Link></li>
               </ul>
             </div>
           </div>
@@ -27,8 +31,8 @@ class Homepage extends Component {
                 <div className="my-5 text-center text-xl-start">
                   <h1 className="display-5 fw-bolder text-white mb-2">A Blackjack game built in React</h1>
                   <p className="lead fw-normal text-white text-start mb-4">Playing Blackjack has never been this easy. Take a seat and try your luck at this blackjack game built in React.js</p>
-                  <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                    <a className="btn btn-primary btn-lg btn-light px-4 me-sm-3 play-button" href="#">Play Now</a>
+                  <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">                    
+                    <Link className="btn btn-primary btn-lg btn-light px-4 me-sm-3 play-button" to="/blackjack">Play Now</Link>
                     <a className="btn btn-outline-light btn-lg px-4" href="#features">Learn More</a>
                   </div>
                 </div>
@@ -38,7 +42,7 @@ class Homepage extends Component {
               </div> */}
               <div className="col-lg-5 d-none d-xl-block text-center">
                 <div className="embed-responsive embed-responsive-16by9">
-                  <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowFullScreen></iframe>
+                  <iframe title="video-demo" className="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowFullScreen></iframe>
                 </div>
               </div>
             </div>
@@ -51,33 +55,50 @@ class Homepage extends Component {
               <div className="col-lg-4 mb-5 mb-lg-0"><h2 className="fw-bolder mb-0">A better way to play Blackjack</h2></div>
               <div className="col-lg-8">
                 <h3>Project Details</h3>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">A Blackjack game built using React.js</li>
-                  <li className="list-group-item">On initial load, a new game is started. Simply click "hit" or "stay"</li>
-                  <li className="list-group-item">The objective of the game is to beat the dealer's hand without going over 21</li>
-                  <li className="list-group-item">If you go over 21, it's a "bust" and you lose. If the dealer goes over 21, you win</li>
-                  <li className="list-group-item">Click "New Game" to start a new game</li>
+                <ul className="list-group list-group-flush my-4">
+                  <li className="list-group-item"><span><FaHandPointRight /></span> A Blackjack game built using React.js</li>
+                  <li className="list-group-item"><span><FaHandPointRight /></span> On initial load, a new game is started. Simply click "hit" or "stay"</li>
+                  <li className="list-group-item"><span><FaHandPointRight /></span> The objective of the game is to beat the dealer's hand without going over 21</li>
+                  <li className="list-group-item"><span><FaHandPointRight /></span> If you go over 21, it's a "bust" and you lose. If the dealer goes over 21, you win</li>
+                  <li className="list-group-item"><span><FaHandPointRight /></span> Click "New Game" to start a new game</li>
                 </ul>
               </div>
             </div>
           </div>
         </section>
 
-        <div className="py-5 bg-light">
+        <div className="py-1 bg-light">
           <div className="container px-5 my-5">
             <div className="row gx-5 justify-content-center">
               <div className="col-lg-4 mb-5">
-                <img src="https://i.imgur.com/xlQkUaE.jpg" class="img-fluid biopic" alt="Image of developer" />
+                <img src="https://i.imgur.com/xlQkUaE.jpg" className="img-fluid biopic" alt="Developer of this app" />
               </div>
-                <div className="col-lg-8 mb-5">
-                  Bio
-                </div>
+              <div className="col-lg-8 mb-5 biotext">
+                <h3>Your Host This Evening</h3>
+                <h5 className="my-4">Eyad Alshafi</h5>
+                <p>Welcome one and all to my humble project. My name is Eyad, and I created this Blackjack 
+                game as part of the Mintbean hackathaon. I'm a software developer with experience in a wide array of 
+                technologies including: JavaScript, React, jQuery, Node, Express, Python, Flask, SQL, and many more. 
+                You can learn more about me by visiting <a href="https://eyad-a.github.io/portfolio/" target="_blank" rel="noreferrer">my portfolio</a>.</p>
+                
+                <p className="text-center my-5">
+                  <span>
+                    <a href="https://www.linkedin.com/in/eyad-alshafi/" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+                  </span>
+                  <span className="mx-5">
+                    <a href="https://github.com/Eyad-A/" target="_blank" rel="noreferrer"><FaGithub /></a>
+                  </span>
+                  <span>
+                    <a href="https://eyad-a.github.io/portfolio/" target="_blank" rel="noreferrer"><FaExternalLinkAlt /></a>
+                  </span>
+                </p>
               </div>
             </div>
-          </div>          
-      </main>      
-        );
+          </div>
+        </div>
+      </main>
+    );
   }
 }
 
-        export default Homepage;
+export default Homepage;
